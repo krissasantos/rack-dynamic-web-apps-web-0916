8a8988b43583cd6a0c7746a9800a39218f949d5b
krissasantos <krissa.santos31@gmail.com>
2016-10-12 17:26:04 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -4,6 +4,18 @@ class Application
     resp = Rack::Response.new
     resp.write "Hello, World"
     resp.finish
-  end
 
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
+ 
+      if num_1==num_2 && num_2==num_3
+        # puts "You Win"
+        resp.write "You Win"
+      else
+        # puts "You Lose"
+        resp.write "You Lose"
+      end
+      resp.finish
+  end
 end
